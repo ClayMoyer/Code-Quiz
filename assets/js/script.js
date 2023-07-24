1b3ea288f3c470a145ac1f8a9389d9c8434d2ea0
Clay_MM <clay.moyer.1515@gmail.com>
2023-07-24 17:41:14 -0400

    javascript functionality save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,35 @@
+let question = $("#question");
+let timer = $("#timer");
+let answerChoices = $("#answerChoices");
+let startButton = $("#startButton");
+let quizHead = $("#quizHead");
+let scoreList = $("#scoreList");
+let timeRemaining = 60;
+let score = 0;
+let getHighScore = JSON.parse(localStorage.getItem('highScores') || "[]");
+let submitScore = $("<button></button>");
+let submitName = $("<input>");
+let timeout;
+
+let questionList = [];
+let activeQuestion;
+class Question {
+    constructor(question, answerA, answerB, answerC, correctAnswer) {
+        this.question = question;
+        this.choices = [answerA, answerB, answerC, correctAnswer];
+        this.correctAnswer = correctAnswer;
+    }
+    setQuestion() {
+        question.text(this.question);
+    }
+    setAnswerChoices() {
+        answerChoices.empty();
+        answerChoices.off();
+        this.options.sort((a, b) => Math.floor(Math.random() * this.options.length));
+        //double check functionality
+    }
+}
+
 function viewHighScores() {
     window.location.href = "highScores.html";
 }
@@ -5,3 +37,4 @@ function viewHighScores() {
 function viewHome() {
     window.location.href = "index.html";
 }
+
